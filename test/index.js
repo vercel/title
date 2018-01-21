5c94240efef54a78184954870d9a004b919aea3f
Leo Lamprecht <mindrun@icloud.com>
2018-01-21 22:37:02 +0100

    Added tests

---
+++ test/index.js
@@ -0,0 +1,12 @@
+// Packages
+const test = require('ava')
+
+// Source
+const title = require('../')
+
+test(t => {
+  const from = 'aPi 2.0: lOG-in with zeit, new dOCs & more'
+  const to = 'API 2.0: Log-In with ZEIT, New Docs & More'
+
+  t.is(title(from), to)
+})
