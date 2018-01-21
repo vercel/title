9420ae8fd70a5684f3669400f8da04006a7042c3
Leo Lamprecht <mindrun@icloud.com>
2018-01-21 23:31:34 +0100

    Took a lot of masks off

---
+++ pages/index.js
@@ -0,0 +1,3 @@
+import title from '../module'
+
+export default () => <span>{title('aPi 2.0: lOG-in with zeit, new dOCs & more')}</span>
--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
 const test = require('ava')
 
 // Source
-const title = require('../')
+const title = require('../lib')
 
 test(t => {
   const from = 'aPi 2.0: lOG-in with zeit, new dOCs & more'
