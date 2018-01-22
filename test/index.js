94cc69cc0f328f9f46e7bd8683be023dd6baa6f1
Leo Lamprecht <mindrun@icloud.com>
2018-01-22 10:48:09 +0100

    Make product casing happen properly

--- test/index.js
+++ test/index.js
@@ -10,3 +10,25 @@ test(t => {
 
   t.is(title(from), to)
 })
+
+test(t => {
+  const from = 'updates TO hAndLinG of Failed paYMEnts'
+  const to = 'Updates to Handling of Failed Payments'
+
+  t.is(title(from), to)
+})
+
+
+test(t => {
+  const from = 'toWArds NEXT.JS 5: Introducing cANaRY Updates'
+  const to = 'Towards Next.js 5: Introducing Canary Updates'
+
+  t.is(title(from), to)
+})
+
+test(t => {
+  const from = 'noW deSktop and now cLI are prODUCts of zeIt'
+  const to = 'Now Desktop and Now CLI Are Products of ZEIT'
+
+  t.is(title(from), to)
+})
